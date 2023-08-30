dbd9c9a4efa606285960504e96625d302e279ce6
v.shamsheev <v.shamsheev@corp.mail.ru>
2023-08-30 19:01:41 +0300

    font-awesome

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 import './App.css';
 import { SignUp } from './pages/sign-up';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+
+library.add(faCoffee);
 
 function App() {
   return (
--- src/pages/sign-up.tsx
+++ src/pages/sign-up.tsx
@@ -2,11 +2,16 @@ import { MainTemplate } from '#ui/templates/main-template';
 import { Title } from '#ui/title/title';
 import { BackLink } from '#features/back-link/back-link';
 import { SignUpForm } from '#features/sign-up-form/sign-up-form';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export const SignUp: React.FC = () => {
   return (
     <MainTemplate
-      header={<header style={{ width: '100%', height: '40px' }}>Header</header>}
+      header={
+        <header style={{ width: '100%', height: '40px' }}>
+          Header <FontAwesomeIcon icon="coffee" />
+        </header>
+      }
       backLink={<BackLink />}
       title={<Title>Sign Up</Title>}
       body={<SignUpForm />}
